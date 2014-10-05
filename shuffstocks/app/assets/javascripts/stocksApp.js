1ce4808496de8e79a8c280140af2a62c974627ff
shuffguy <ianvshuff@gmail.com>
2014-10-05 15:03:13 -0400

    shuff

--- shuffstocks/app/assets/javascripts/stocksApp.js
+++ shuffstocks/app/assets/javascripts/stocksApp.js
@@ -1,7 +1,9 @@
 var stocksApp = angular.module('stocksApp', []);
 
-stocksApp.controller("stocksCtrl", ["$scope", "$filter",function($scope) {
+stocksApp.controller("stocksCtrl", ["$scope", "$filter", function($scope) {
 
 	$scope.stocksArray = "Thsi is working!ddddd"
+
+	
 
 }]);
